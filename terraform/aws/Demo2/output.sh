5f11855842c4610c5146cd27d2264bd86eecfd4c
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2021-01-17 18:42:13 +0800

    update tags for sg rules

--- terraform/aws/Demo2/output.sh
+++ terraform/aws/Demo2/output.sh
@@ -198,3 +198,160 @@ aws_instance.web: Creation complete after 13s [id=i-025275add796f3a4b]
 
 Apply complete! Resources: 2 added, 0 changed, 0 destroyed.
 bharathkumarraju@R77-NB193 Demo2 %
+
+bharathkumarraju@R77-NB193 Demo2 % terraform init
+
+Initializing the backend...
+
+Initializing provider plugins...
+
+The following providers do not have any version constraints in configuration,
+so the latest version was installed.
+
+To prevent automatic upgrades to new major versions that may contain breaking
+changes, it is recommended to add version = "..." constraints to the
+corresponding provider blocks in configuration, with the constraint strings
+suggested below.
+
+* provider.aws: version = "~> 3.24"
+
+Terraform has been successfully initialized!
+
+You may now begin working with Terraform. Try running "terraform plan" to see
+any changes that are required for your infrastructure. All Terraform commands
+should now work.
+
+If you ever set or change modules or backend configuration for Terraform,
+rerun this command to reinitialize your working directory. If you forget, other
+commands will detect it and remind you to do so if necessary.
+bharathkumarraju@R77-NB193 Demo2 % terraform plan
+Refreshing Terraform state in-memory prior to plan...
+The refreshed state will be used to calculate this plan, but will not be
+persisted to local or remote state storage.
+
+aws_security_group.web: Refreshing state... [id=sg-03abe64e154f6ad5a]
+aws_instance.web: Refreshing state... [id=i-025275add796f3a4b]
+
+------------------------------------------------------------------------
+
+An execution plan has been generated and is shown below.
+Resource actions are indicated with the following symbols:
+  ~ update in-place
+
+Terraform will perform the following actions:
+
+  # aws_security_group.web will be updated in-place
+  ~ resource "aws_security_group" "web" {
+        arn                    = "arn:aws:ec2:ap-southeast-1:172586632398:security-group/sg-03abe64e154f6ad5a"
+        description            = "SG for web server"
+      ~ egress                 = [
+          - {
+              - cidr_blocks      = [
+                  - "0.0.0.0/0",
+                ]
+              - description      = ""
+              - from_port        = 0
+              - ipv6_cidr_blocks = []
+              - prefix_list_ids  = []
+              - protocol         = "-1"
+              - security_groups  = []
+              - self             = false
+              - to_port          = 0
+            },
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = "allow all ports"
+              + from_port        = 0
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "-1"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 0
+            },
+        ]
+        id                     = "sg-03abe64e154f6ad5a"
+      ~ ingress                = [
+          - {
+              - cidr_blocks      = [
+                  - "0.0.0.0/0",
+                ]
+              - description      = ""
+              - from_port        = 443
+              - ipv6_cidr_blocks = []
+              - prefix_list_ids  = []
+              - protocol         = "tcp"
+              - security_groups  = []
+              - self             = false
+              - to_port          = 443
+            },
+          - {
+              - cidr_blocks      = [
+                  - "0.0.0.0/0",
+                ]
+              - description      = ""
+              - from_port        = 80
+              - ipv6_cidr_blocks = []
+              - prefix_list_ids  = []
+              - protocol         = "tcp"
+              - security_groups  = []
+              - self             = false
+              - to_port          = 80
+            },
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = "allow port HTTP"
+              + from_port        = 80
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "tcp"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 80
+            },
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = "allow port HTTPS"
+              + from_port        = 443
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "tcp"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 443
+            },
+        ]
+        name                   = "webserver-SG"
+        owner_id               = "172586632398"
+        revoke_rules_on_delete = false
+        tags                   = {
+            "Name"  = "WebServer SG"
+            "Owner" = "Bharath raju"
+        }
+        vpc_id                 = "vpc-83c5f8e7"
+    }
+
+Plan: 0 to add, 1 to change, 0 to destroy.
+
+------------------------------------------------------------------------
+
+Note: You didn't specify an "-out" parameter to save this plan, so Terraform
+can't guarantee that exactly these actions will be performed if
+"terraform apply" is subsequently run.
+
+bharathkumarraju@R77-NB193 Demo2 %
+
+bharathkumarraju@R77-NB193 Demo2 % terraform apply -auto-approve
+aws_security_group.web: Refreshing state... [id=sg-03abe64e154f6ad5a]
+aws_instance.web: Refreshing state... [id=i-025275add796f3a4b]
+aws_security_group.web: Modifying... [id=sg-03abe64e154f6ad5a]
+aws_security_group.web: Modifications complete after 10s [id=sg-03abe64e154f6ad5a]
+
+Apply complete! Resources: 0 added, 1 changed, 0 destroyed.
+bharathkumarraju@R77-NB193 Demo2 %
