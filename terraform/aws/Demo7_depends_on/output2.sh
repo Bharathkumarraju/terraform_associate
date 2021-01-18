c5378d0533e81bd05d4dc5dff30ca435d7788458
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2021-01-19 07:55:32 +0800

    update all tf names

---
+++ terraform/aws/Demo7_depends_on/output2.sh
@@ -0,0 +1,415 @@
+bharathkumarraju@R77-NB193 Demo7_depends_on % terraform plan
+Refreshing Terraform state in-memory prior to plan...
+The refreshed state will be used to calculate this plan, but will not be
+persisted to local or remote state storage.
+
+
+------------------------------------------------------------------------
+
+An execution plan has been generated and is shown below.
+Resource actions are indicated with the following symbols:
+  + create
+
+Terraform will perform the following actions:
+
+  # aws_instance.app will be created
+  + resource "aws_instance" "app" {
+      + ami                          = "ami-00b8d9cb8a7161e41"
+      + arn                          = (known after apply)
+      + associate_public_ip_address  = (known after apply)
+      + availability_zone            = (known after apply)
+      + cpu_core_count               = (known after apply)
+      + cpu_threads_per_core         = (known after apply)
+      + get_password_data            = false
+      + host_id                      = (known after apply)
+      + id                           = (known after apply)
+      + instance_state               = (known after apply)
+      + instance_type                = "t3.micro"
+      + ipv6_address_count           = (known after apply)
+      + ipv6_addresses               = (known after apply)
+      + key_name                     = (known after apply)
+      + outpost_arn                  = (known after apply)
+      + password_data                = (known after apply)
+      + placement_group              = (known after apply)
+      + primary_network_interface_id = (known after apply)
+      + private_dns                  = (known after apply)
+      + private_ip                   = (known after apply)
+      + public_dns                   = (known after apply)
+      + public_ip                    = (known after apply)
+      + secondary_private_ips        = (known after apply)
+      + security_groups              = (known after apply)
+      + source_dest_check            = true
+      + subnet_id                    = (known after apply)
+      + tags                         = {
+          + "Name"  = "bk_app"
+          + "Owner" = "Bharath raju"
+        }
+      + tenancy                      = (known after apply)
+      + vpc_security_group_ids       = (known after apply)
+
+      + ebs_block_device {
+          + delete_on_termination = (known after apply)
+          + device_name           = (known after apply)
+          + encrypted             = (known after apply)
+          + iops                  = (known after apply)
+          + kms_key_id            = (known after apply)
+          + snapshot_id           = (known after apply)
+          + tags                  = (known after apply)
+          + throughput            = (known after apply)
+          + volume_id             = (known after apply)
+          + volume_size           = (known after apply)
+          + volume_type           = (known after apply)
+        }
+
+      + enclave_options {
+          + enabled = (known after apply)
+        }
+
+      + ephemeral_block_device {
+          + device_name  = (known after apply)
+          + no_device    = (known after apply)
+          + virtual_name = (known after apply)
+        }
+
+      + metadata_options {
+          + http_endpoint               = (known after apply)
+          + http_put_response_hop_limit = (known after apply)
+          + http_tokens                 = (known after apply)
+        }
+
+      + network_interface {
+          + delete_on_termination = (known after apply)
+          + device_index          = (known after apply)
+          + network_interface_id  = (known after apply)
+        }
+
+      + root_block_device {
+          + delete_on_termination = (known after apply)
+          + device_name           = (known after apply)
+          + encrypted             = (known after apply)
+          + iops                  = (known after apply)
+          + kms_key_id            = (known after apply)
+          + tags                  = (known after apply)
+          + throughput            = (known after apply)
+          + volume_id             = (known after apply)
+          + volume_size           = (known after apply)
+          + volume_type           = (known after apply)
+        }
+    }
+
+  # aws_instance.db will be created
+  + resource "aws_instance" "db" {
+      + ami                          = "ami-00b8d9cb8a7161e41"
+      + arn                          = (known after apply)
+      + associate_public_ip_address  = (known after apply)
+      + availability_zone            = (known after apply)
+      + cpu_core_count               = (known after apply)
+      + cpu_threads_per_core         = (known after apply)
+      + get_password_data            = false
+      + host_id                      = (known after apply)
+      + id                           = (known after apply)
+      + instance_state               = (known after apply)
+      + instance_type                = "t3.micro"
+      + ipv6_address_count           = (known after apply)
+      + ipv6_addresses               = (known after apply)
+      + key_name                     = (known after apply)
+      + outpost_arn                  = (known after apply)
+      + password_data                = (known after apply)
+      + placement_group              = (known after apply)
+      + primary_network_interface_id = (known after apply)
+      + private_dns                  = (known after apply)
+      + private_ip                   = (known after apply)
+      + public_dns                   = (known after apply)
+      + public_ip                    = (known after apply)
+      + secondary_private_ips        = (known after apply)
+      + security_groups              = (known after apply)
+      + source_dest_check            = true
+      + subnet_id                    = (known after apply)
+      + tags                         = {
+          + "Name"  = "bk_db"
+          + "Owner" = "Bharath raju"
+        }
+      + tenancy                      = (known after apply)
+      + vpc_security_group_ids       = (known after apply)
+
+      + ebs_block_device {
+          + delete_on_termination = (known after apply)
+          + device_name           = (known after apply)
+          + encrypted             = (known after apply)
+          + iops                  = (known after apply)
+          + kms_key_id            = (known after apply)
+          + snapshot_id           = (known after apply)
+          + tags                  = (known after apply)
+          + throughput            = (known after apply)
+          + volume_id             = (known after apply)
+          + volume_size           = (known after apply)
+          + volume_type           = (known after apply)
+        }
+
+      + enclave_options {
+          + enabled = (known after apply)
+        }
+
+      + ephemeral_block_device {
+          + device_name  = (known after apply)
+          + no_device    = (known after apply)
+          + virtual_name = (known after apply)
+        }
+
+      + metadata_options {
+          + http_endpoint               = (known after apply)
+          + http_put_response_hop_limit = (known after apply)
+          + http_tokens                 = (known after apply)
+        }
+
+      + network_interface {
+          + delete_on_termination = (known after apply)
+          + device_index          = (known after apply)
+          + network_interface_id  = (known after apply)
+        }
+
+      + root_block_device {
+          + delete_on_termination = (known after apply)
+          + device_name           = (known after apply)
+          + encrypted             = (known after apply)
+          + iops                  = (known after apply)
+          + kms_key_id            = (known after apply)
+          + tags                  = (known after apply)
+          + throughput            = (known after apply)
+          + volume_id             = (known after apply)
+          + volume_size           = (known after apply)
+          + volume_type           = (known after apply)
+        }
+    }
+
+  # aws_instance.web will be created
+  + resource "aws_instance" "web" {
+      + ami                          = "ami-00b8d9cb8a7161e41"
+      + arn                          = (known after apply)
+      + associate_public_ip_address  = (known after apply)
+      + availability_zone            = (known after apply)
+      + cpu_core_count               = (known after apply)
+      + cpu_threads_per_core         = (known after apply)
+      + get_password_data            = false
+      + host_id                      = (known after apply)
+      + id                           = (known after apply)
+      + instance_state               = (known after apply)
+      + instance_type                = "t3.micro"
+      + ipv6_address_count           = (known after apply)
+      + ipv6_addresses               = (known after apply)
+      + key_name                     = (known after apply)
+      + outpost_arn                  = (known after apply)
+      + password_data                = (known after apply)
+      + placement_group              = (known after apply)
+      + primary_network_interface_id = (known after apply)
+      + private_dns                  = (known after apply)
+      + private_ip                   = (known after apply)
+      + public_dns                   = (known after apply)
+      + public_ip                    = (known after apply)
+      + secondary_private_ips        = (known after apply)
+      + security_groups              = (known after apply)
+      + source_dest_check            = true
+      + subnet_id                    = (known after apply)
+      + tags                         = {
+          + "Name"  = "bk_web"
+          + "Owner" = "Bharath raju"
+        }
+      + tenancy                      = (known after apply)
+      + vpc_security_group_ids       = (known after apply)
+
+      + ebs_block_device {
+          + delete_on_termination = (known after apply)
+          + device_name           = (known after apply)
+          + encrypted             = (known after apply)
+          + iops                  = (known after apply)
+          + kms_key_id            = (known after apply)
+          + snapshot_id           = (known after apply)
+          + tags                  = (known after apply)
+          + throughput            = (known after apply)
+          + volume_id             = (known after apply)
+          + volume_size           = (known after apply)
+          + volume_type           = (known after apply)
+        }
+
+      + enclave_options {
+          + enabled = (known after apply)
+        }
+
+      + ephemeral_block_device {
+          + device_name  = (known after apply)
+          + no_device    = (known after apply)
+          + virtual_name = (known after apply)
+        }
+
+      + metadata_options {
+          + http_endpoint               = (known after apply)
+          + http_put_response_hop_limit = (known after apply)
+          + http_tokens                 = (known after apply)
+        }
+
+      + network_interface {
+          + delete_on_termination = (known after apply)
+          + device_index          = (known after apply)
+          + network_interface_id  = (known after apply)
+        }
+
+      + root_block_device {
+          + delete_on_termination = (known after apply)
+          + device_name           = (known after apply)
+          + encrypted             = (known after apply)
+          + iops                  = (known after apply)
+          + kms_key_id            = (known after apply)
+          + tags                  = (known after apply)
+          + throughput            = (known after apply)
+          + volume_id             = (known after apply)
+          + volume_size           = (known after apply)
+          + volume_type           = (known after apply)
+        }
+    }
+
+  # aws_security_group.general will be created
+  + resource "aws_security_group" "general" {
+      + arn                    = (known after apply)
+      + description            = "Managed by Terraform"
+      + egress                 = [
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = ""
+              + from_port        = 0
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "-1"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 0
+            },
+        ]
+      + id                     = (known after apply)
+      + ingress                = [
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = ""
+              + from_port        = 22
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "tcp"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 22
+            },
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = ""
+              + from_port        = 3389
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "tcp"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 3389
+            },
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = ""
+              + from_port        = 443
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "tcp"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 443
+            },
+          + {
+              + cidr_blocks      = [
+                  + "0.0.0.0/0",
+                ]
+              + description      = ""
+              + from_port        = 80
+              + ipv6_cidr_blocks = []
+              + prefix_list_ids  = []
+              + protocol         = "tcp"
+              + security_groups  = []
+              + self             = false
+              + to_port          = 80
+            },
+        ]
+      + name                   = "bksg"
+      + owner_id               = (known after apply)
+      + revoke_rules_on_delete = false
+      + tags                   = {
+          + "Name" = "bharaths_sg"
+        }
+      + vpc_id                 = (known after apply)
+    }
+
+Plan: 4 to add, 0 to change, 0 to destroy.
+
+------------------------------------------------------------------------
+
+Note: You didn't specify an "-out" parameter to save this plan, so Terraform
+can't guarantee that exactly these actions will be performed if
+"terraform apply" is subsequently run.
+
+bharathkumarraju@R77-NB193 Demo7_depends_on % terraform apply -auto-approve
+aws_security_group.general: Creating...
+aws_security_group.general: Creation complete after 2s [id=sg-0896ce4a665260a92]
+aws_instance.db: Creating...
+
+bharathkumarraju@R77-NB193 Demo7_depends_on % terraform apply -auto-approve
+aws_security_group.general: Creating...
+aws_security_group.general: Creation complete after 2s [id=sg-0896ce4a665260a92]
+aws_instance.db: Creating...
+aws_instance.db: Still creating... [10s elapsed]
+aws_instance.db: Creation complete after 12s [id=i-0f28fed46934c8a2d]
+aws_instance.app: Creating...
+aws_instance.app: Still creating... [10s elapsed]
+aws_instance.app: Creation complete after 13s [id=i-09f609474a9eecf91]
+aws_instance.web: Creating...
+aws_instance.web: Still creating... [10s elapsed]
+aws_instance.web: Creation complete after 16s [id=i-09d4f27bf18f2300b]
+
+Apply complete! Resources: 4 added, 0 changed, 0 destroyed.
+bharathkumarraju@R77-NB193 Demo7_depends_on %
+
+
+bharathkumarraju@R77-NB193 Demo7_depends_on % terraform destroy -auto-approve
+aws_security_group.general: Refreshing state... [id=sg-0896ce4a665260a92]
+aws_instance.db: Refreshing state... [id=i-0f28fed46934c8a2d]
+aws_instance.app: Refreshing state... [id=i-09f609474a9eecf91]
+aws_instance.web: Refreshing state... [id=i-09d4f27bf18f2300b]
+aws_instance.web: Destroying... [id=i-09d4f27bf18f2300b]
+aws_instance.web: Still destroying... [id=i-09d4f27bf18f2300b, 10s elapsed]
+aws_instance.web: Still destroying... [id=i-09d4f27bf18f2300b, 20s elapsed]
+aws_instance.web: Destruction complete after 30s
+aws_instance.app: Destroying... [id=i-09f609474a9eecf91]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 10s elapsed]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 20s elapsed]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 30s elapsed]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 40s elapsed]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 50s elapsed]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 1m0s elapsed]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 1m10s elapsed]
+aws_instance.app: Still destroying... [id=i-09f609474a9eecf91, 1m20s elapsed]
+aws_instance.app: Destruction complete after 1m20s
+aws_instance.db: Destroying... [id=i-0f28fed46934c8a2d]
+aws_instance.db: Still destroying... [id=i-0f28fed46934c8a2d, 10s elapsed]
+aws_instance.db: Still destroying... [id=i-0f28fed46934c8a2d, 20s elapsed]
+aws_instance.db: Still destroying... [id=i-0f28fed46934c8a2d, 30s elapsed]
+aws_instance.db: Still destroying... [id=i-0f28fed46934c8a2d, 40s elapsed]
+aws_instance.db: Still destroying... [id=i-0f28fed46934c8a2d, 50s elapsed]
+aws_instance.db: Destruction complete after 50s
+aws_security_group.general: Destroying... [id=sg-0896ce4a665260a92]
+aws_security_group.general: Destruction complete after 1s
+
+Destroy complete! Resources: 4 destroyed.
+bharathkumarraju@R77-NB193 Demo7_depends_on %
+
+
---
+++ terraform/aws/Demo8_outputs/main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-00b8d9cb8a7161e41" //amazon linux
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.general.id]
+  tags = {
+    Name  = "bk_web"
+    Owner = "Bharath raju"
+  }
+  depends_on = [aws_instance.db, aws_instance.app]
+}
+resource "aws_instance" "app" {
+  ami = "ami-00b8d9cb8a7161e41" //amazon linux
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.general.id]
+  tags = {
+    Name  = "bk_app"
+    Owner = "Bharath raju"
+  }
+  depends_on = [aws_instance.db]
+}
+
+resource "aws_instance" "db" {
+  ami = "ami-00b8d9cb8a7161e41" //amazon linux
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.general.id]
+  tags = {
+    Name  = "bk_db"
+    Owner = "Bharath raju"
+  }
+}
+
+resource "aws_security_group" "general" {
+  name = "bksg"
+  dynamic "ingress" {
+    for_each = ["80", "443", "22", "3389"]
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "bharaths_sg"
+  }
+}
+
+#-------------outputs-------------------------------------
+
+output "bharath_sg_id" {
+  description = "SG id for my server"
+  value = aws_security_group.general.id
+}
+
+output "bharath_sg_all_details" {
+  description = "all details of SG"
+  value = aws_security_group.general
+}
