0313bb95f85574dca7e1290d6cc4413d0eb777d5
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2021-02-12 16:47:34 +0800

    Run terraform on multiple AWS accounts

---
+++ terraform/aws/Demo24_multiple_aws_accounts/demo_output.sh
@@ -0,0 +1,2 @@
+another aws account ARN: arn:aws:iam::928157669239:role/TerraformMultiAccountRole
+
--- terraform/aws/Demo24_multiple_aws_accounts/main.tf
+++ terraform/aws/Demo24_multiple_aws_accounts/main.tf
@@ -1,1 +1,28 @@
-#
+provider "aws" {
+  region = "eu-west-2"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+  alias = "UAT"
+#  access_key = ""
+#  secret_key = ""
+  assume_role {
+    role_arn = "arn:aws:iam::928157669239:role/TerraformMultiAccountRole"
+  }
+}
+
+resource "aws_vpc" "bharath_master_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Bharath Master VPC"
+  }
+}
+
+resource "aws_vpc" "uat_vpc" {
+  provider = aws.UAT
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Bharath UAT VPC"
+  }
+}
