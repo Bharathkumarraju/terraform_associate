58053f0b0047cb137b47c266368caaccde179310
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2021-02-14 16:10:48 +0800

    module for ex2 server

--- terraform/aws/Demo26_TF_modules1/ProjectB/main.tf
+++ terraform/aws/Demo26_TF_modules1/ProjectB/main.tf
@@ -1,1 +1,24 @@
-#
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "bharath_vpc_production" {
+  source = "../modules/aws_network"
+  env  = "production"
+  vpc_cidr = "10.200.0.0/16"
+  public_subnet_cidrs = ["10.200.1.0/24","10.200.2.0/24","10.200.3.0/24"]
+  private_subnet_cidrs = ["10.200.11.0/24","10.200.12.0/24","10.200.13.0/24"]
+  tags = {
+    Owner = "BKRAJU"
+    Code = "18181818"
+    Project = "BKRPROD"
+  }
+}
+
+module "prod_server" {
+  source = "../modules/aws_testserver"
+  name = "BK-PROD"
+  message =  "standalone server"
+  subnet_id = module.bharath_vpc_production.public_subnet_ids[2]
+}
---
+++ terraform/aws/Demo26_TF_modules1/ProjectB/outputs.tf
@@ -0,0 +1,3 @@
+output "server_ip" {
+  value = module.prod_server.web_server_public_ip
+}
